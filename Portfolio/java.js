c4a65e01f3aed763d5128589d1d7a862c5c92e43
Kullery <duhesme.louis@gmail.com>
2024-10-31 16:39:44 +0100

    Add files via upload

---
+++ Portfolio/java.js
@@ -0,0 +1,17 @@
+function liencv(){
+    window.open('cv-duhesme-louis.pdf', '_blank');
+}
+
+function liencertifs(){
+    window.location.href = 'certifs.html';
+}
+function retour(){
+    window.location.href = 'index.html';
+}
+function lienprojets(){
+    window.location.href = 'Projetvf/index.html';
+}
+
+function back1(){
+    window.location.href = '../index.html';
+}
